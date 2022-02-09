79888080c04d36abade90f3e9e394f93018ae732
ASPIREFabio <fabiosimedo@hotmail.com>
2022-02-09 15:39:35 -0300

    Connection estabilished with mongodb Atlas

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true })
+
+const conn = mongoose.connection
+conn.once('open', () => console.log(`MongoDB connected at ${conn.name}`))
+
+app.listen(port, () => console.log(`Server running in por ${port}`))
